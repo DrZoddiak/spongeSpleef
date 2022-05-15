083e26a016de44fe684cc309305a59a4497db3a4
DrZoddiak <JesseMcKee97@gmail.com>
2022-05-14 19:02:22 -0500

    Flesh out GameSettings

--- src/main/java/me/zodd/spleef/game/GameSettings.java
+++ src/main/java/me/zodd/spleef/game/GameSettings.java
@@ -1,15 +1,73 @@
 package me.zodd.spleef.game;
 
+import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameSettings {
-    boolean pvpAllowed = false;
+    private boolean pvpAllowed;
+    private boolean resetArenaAutomatically;
+    private int maxPlayers;
+    private List<BlockType> floorBlocks;
+    private ItemType startingItem;
+    private int matchTime;
 
-    boolean resetArenaAutomatically = true;
-
-    int maxPlayers = 2;
-
-
-
-    public void setPvpAllowed(boolean pvpAllowed) {
+    public GameSettings gameSettings(
+            boolean pvpAllowed,
+            boolean resetArenaAutomatically,
+            int maxPlayers,
+            List<BlockType> floorBlocks,
+            ItemType startingItem,
+            int matchTime
+    ) {
         this.pvpAllowed = pvpAllowed;
+        this.resetArenaAutomatically = resetArenaAutomatically;
+        this.maxPlayers = maxPlayers;
+        this.floorBlocks = floorBlocks;
+        this.startingItem = startingItem;
+        this.matchTime = matchTime;
+        return this;
+    }
+
+    public GameSettings gameSettings() {
+        List<BlockType> fb = new ArrayList<>();
+        fb.add(BlockTypes.SNOW_BLOCK.get());
+        return gameSettings(
+                false,
+                true,
+                2,
+                fb,
+                ItemTypes.GOLDEN_SHOVEL.get(),
+                300
+        );
+    }
+
+    public int getMatchTime() {
+        return matchTime;
+    }
+
+    public boolean isResetArenaAutomatically() {
+        return resetArenaAutomatically;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public ItemType getStartingItem() {
+        return startingItem;
+    }
+
+    public boolean isPvpAllowed() {
+        return pvpAllowed;
+    }
+
+    public List<BlockType> getFloorBlocks() {
+        if (floorBlocks.isEmpty()) floorBlocks.add(BlockTypes.SNOW_BLOCK.get());
+        return floorBlocks;
     }
 }
