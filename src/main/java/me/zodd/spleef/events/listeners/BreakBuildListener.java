cac020b374dfb8cf1876af533c21dbaf8cb7fef7
iloggedin <arthmorgt2@gmail.com>
2022-05-16 22:06:12 +0200

    Added a for loop for every online player

--- src/main/java/me/zodd/spleef/events/listeners/BreakBuildListener.java
+++ src/main/java/me/zodd/spleef/events/listeners/BreakBuildListener.java
@@ -1,16 +1,23 @@
 package me.zodd.spleef.events.listeners;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.network.status.StatusResponse;
 import org.spongepowered.api.world.server.ServerLocation;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class BreakBuildListener {
-
     @Listener
     public void onBlockModification(ChangeBlockEvent.Pre event) {
-        //TODO Check if players are in game
+        List<Player> users = new ArrayList<>();
+                for(org.spongepowered.api.entity.living.player.Player player : Sponge.server().onlinePlayers()){
+                    users.add(player);
+        }
         //TODO Check if location is inside of arena
         List<ServerLocation> locations = event.locations();
 
